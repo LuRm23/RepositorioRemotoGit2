d7e95db8f1281a1ceb26dbcdd19ad098079b57ab
diurno <diurno@B21>
2024-01-12 12:59:26 +0100

    Subida2

---
+++ Proyecto2/src/Paquete2/Clase2.java
@@ -0,0 +1,11 @@
+package Paquete2;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generate method stub
+		System.out.println("Ejemplo2 de contro de versiones con Git");
+		
+	}
+
+}
